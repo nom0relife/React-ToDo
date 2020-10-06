dcf7f37c3028235fac48762fe92abbaad8b9beac
nom0relife <45268627+nom0relife@users.noreply.github.com>
2020-10-06 23:41:52 +0300

    first commit of project

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navigation() {
+  return (
+    <div className="input-form">
+      <h2>Do Things</h2>
+      <div className="text-container">
+        <input type="text" name="" id="" />
+        <button className="btn btn-secondary">+</button>
+      </div>
+    </div>
+  );
+}
